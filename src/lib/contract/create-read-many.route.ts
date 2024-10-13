a6b00d77c134a091df940c4044de14edf7e826e0
chihabhajji <chihabhajji@protonmail.com>
2024-10-13 14:25:15 +0100

    cleanup

--- src/lib/contract/create-read-many.route.ts
+++ src/lib/contract/create-read-many.route.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import _ from 'lodash';
 
-import type { PaginationOffsetDto } from '../dto';
+import type { PaginationOffsetDto, PaginationRequest } from '../dto';
 import type { ReadManyRouteOption } from './crud-contract.type';
 import type { ContractPlainType, initContract } from '@ts-rest/core';
 import type { EntityType, OffsetPaginationResponse } from 'src';
@@ -8,25 +9,19 @@ import type { EntityType, OffsetPaginationResponse } from 'src';
 export function makeReadManyRoute<T extends EntityType>(
     options: Omit<ReadManyRouteOption, 'method' | 'path' | 'responses'>,
     contract: ReturnType<typeof initContract>,
-    entity: T,
-): ReadManyRouteOption & {
-    path: '/';
-    method: 'GET';
-    query: ContractPlainType<PaginationOffsetDto>;
-    responses: { 200: OffsetPaginationResponse<typeof entity>; 400: { message: string } };
-} {
+) {
     const opts = _.cloneDeep(options) as ReadManyRouteOption;
     opts.path = '/';
     opts.method = 'GET';
-    opts.query = contract.type<PaginationOffsetDto>();
+    opts.query = contract.type<PaginationRequest<T>>();
     opts.responses = {
-        200: contract.type<OffsetPaginationResponse<typeof entity>>(),
+        200: contract.type<OffsetPaginationResponse<T>>(),
         400: contract.type<{ message: string }>(),
     };
     return opts as ReadManyRouteOption & {
         path: '/';
         method: 'GET';
         query: ContractPlainType<PaginationOffsetDto>;
-        responses: { 200: OffsetPaginationResponse<typeof entity>; 400: { message: string } };
+        responses: { 200: OffsetPaginationResponse<T>; 400: { message: string } };
     };
 }
